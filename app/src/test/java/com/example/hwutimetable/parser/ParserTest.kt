6ac96df8177cf7abaa50d41df9bf0e577701eb28
AdamTMalek <malekadas@gmail.com>
2019-11-09 12:02:46 +0000

    Add simple Parser tests

---
+++ app/src/test/java/com/example/hwutimetable/parser/ParserTest.kt
@@ -0,0 +1,31 @@
+package com.example.hwutimetable.parser
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
+import java.io.File
+
+class ParserTest {
+    private val parser: Parser = Parser(
+        org.jsoup.Jsoup.parse(File("src/test/sampleTimetables/tt1.html"), "UTF-8")
+    )
+
+    @Before
+    fun runParser() {
+        parser.parse()
+    }
+
+    /**
+     * Test if the ParserException is throw, when the Parser has a document without a Jsoup parser
+     */
+    @Test(expected = ParserException::class)
+    fun parseNoDocParser() {
+        Parser(org.jsoup.nodes.Document("src/test/sampleTimetables/tt1.html")).parse()
+    }
+
+    @Test
+    fun testNumberOfLectures() {
+        // There are 15 lectures in tt1.html
+        assertEquals(15, parser.lectures.size)
+    }
+}
