3a6233d0a96a7719c63c6ccd35aaaf48dfa942f9
AdamTMalek <malekadas@gmail.com>
2019-11-09 19:31:25 +0000

    Document the DocumentHandler

--- app/src/main/java/com/example/hwutimetable/DocumentHandler.kt
+++ app/src/main/java/com/example/hwutimetable/DocumentHandler.kt
@@ -9,8 +9,20 @@ import java.io.File
 import java.io.IOException
 import kotlinx.serialization.*
 
+/**
+ * This class provides methods that allow saving the JSoup documents as HTML files
+ * and keeps a record of any additional information for a timetable
+ */
 class DocumentHandler {
     companion object {
+        /**
+         * Saves the given [document] (timetable) as an HTML timetable and the [timetableInfo]
+         * in a JSON dictionary so that the [document] name, which is the group code,
+         * can be easily mapped to a readable name of the group.
+         * @param context: Context of the application - used to get the files directory
+         * @param document: JSoup document of the timetable
+         * @param timetableInfo: Information of the timetable (code and name)
+         */
         fun save(context: Context, document: Document, timetableInfo: TimetableInfo) {
             saveTimetableInfo(context, timetableInfo)
             saveTimetable(context, document, timetableInfo.code)
@@ -18,10 +30,14 @@ class DocumentHandler {
 
         private fun saveTimetableInfo(context: Context, timetableInfo: TimetableInfo) {
             val file = File(context.filesDir, "tt_dict.json")
+
+            // First, get the list have so far
             val list = mutableListOf<TimetableInfo>()
             if (file.isFile) {
                 list.addAll(getExistingInfo(file))
             }
+
+            // Add the new info and save
             list.add(timetableInfo)
             saveInfoAsJson(file, list)
         }
@@ -38,6 +54,13 @@ class DocumentHandler {
             file.writeText(jsonData)
         }
 
+        /**
+         * Save the timetable as HTML document with [timetableName] as the name
+         * (without the extension as it is added in the method)
+         * @param context: Context of the application - used to get the files directory
+         * @param document: Timetable to save
+         * @param timetableName: Group code
+         */
         private fun saveTimetable(
             context: Context,
             document: Document,
--- app/src/main/java/com/example/hwutimetable/TimetableInfo.kt
+++ app/src/main/java/com/example/hwutimetable/TimetableInfo.kt
@@ -2,5 +2,11 @@ package com.example.hwutimetable
 
 import kotlinx.serialization.Serializable
 
+/**
+ * TimetableInfo represents information about a timetable
+ * that gets saved to the device.
+ * @param code: Code of the timetable (from the option value from the Timetables website)
+ * @param name: Readable name
+ */
 @Serializable
 data class TimetableInfo(val code: String, val name: String)
