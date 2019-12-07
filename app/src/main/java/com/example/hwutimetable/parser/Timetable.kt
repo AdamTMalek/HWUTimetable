e1e9140959ddd0a08c2e8b517ebef374e0d95e47
AdamTMalek <malekadas@gmail.com>
2019-12-07 22:07:23 +0000

    Simplify getTotalItems() by using sumBy

--- app/src/main/java/com/example/hwutimetable/parser/Timetable.kt
+++ app/src/main/java/com/example/hwutimetable/parser/Timetable.kt
@@ -1,15 +1,6 @@
 package com.example.hwutimetable.parser
 
 
-class Timetable(
-    val hash: ByteArray,
-    val days: Array<TimetableDay>
-) {
-    fun getTotalItems(): Int {
-        var items = 0
-        days.forEach {day ->
-            items += day.items.size
-        }
-        return items
-    }
+class Timetable(val hash: ByteArray, val days: Array<TimetableDay>) {
+    fun getTotalItems() = days.sumBy { it.items.size }
 }
