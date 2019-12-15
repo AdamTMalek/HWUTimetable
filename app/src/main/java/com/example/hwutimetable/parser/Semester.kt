1f22c2334325e2971b7f32de176794b2950a9d8f
AdamTMalek <malekadas@gmail.com>
2019-12-15 13:16:50 +0000

    Fix joda no-args exception while reading the json timetable

--- app/src/main/java/com/example/hwutimetable/parser/Semester.kt
+++ app/src/main/java/com/example/hwutimetable/parser/Semester.kt
@@ -4,11 +4,28 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.joda.time.LocalDate
 import org.joda.time.Weeks
+import java.io.Serializable
 
 
 @Parcelize
-class Semester(val startDate: LocalDate) : Parcelable {
+class Semester(val startDate: LocalDate) : Parcelable, Serializable {
     fun getCurrentWeek(currentDate: LocalDate): Int {
         return Weeks.weeksBetween(startDate, currentDate).weeks + 1
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Semester
+
+        if (startDate != other.startDate) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return startDate.hashCode()
+    }
+
 }
