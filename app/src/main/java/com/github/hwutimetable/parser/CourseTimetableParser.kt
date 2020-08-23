44c7035db8e9fe8d0b6c8ff9c437c8fd97bf746b
AdamTMalek <malekadas@gmail.com>
2020-08-23 19:50:26 +0100

    Make course code and name var

--- app/src/main/java/com/github/hwutimetable/parser/CourseTimetableParser.kt
+++ app/src/main/java/com/github/hwutimetable/parser/CourseTimetableParser.kt
@@ -4,10 +4,9 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 class CourseTimetableParser(
-    private val courseCode: String, private val courseName: String,
+    var courseCode: String, var courseName: String,
     document: Document?, typeBackgroundProvider: TimetableClass.Type.BackgroundProvider
-) :
-    Parser(document, typeBackgroundProvider) {
+) : Parser(document, typeBackgroundProvider) {
     override fun getCode(classInfoTables: Elements): String {
         return courseCode
     }
