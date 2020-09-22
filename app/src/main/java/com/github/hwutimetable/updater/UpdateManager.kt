91da1633b3780812275eb48be66bab061f6529e7
AdamTMalek <malekadas@gmail.com>
2020-09-22 20:42:23 +0100

    Cleanup update manager. Ensure consistency with default values of preferences.

--- app/src/main/java/com/github/hwutimetable/updater/UpdateManager.kt
+++ app/src/main/java/com/github/hwutimetable/updater/UpdateManager.kt
@@ -21,7 +21,9 @@ internal class UpdateManager(private val context: Context) :
     private val logTag = "UpdateManager"
     private val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
     private val alarmManager: AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    private val updaterIntent: PendingIntent  // UpdateService intent
+    private val updaterIntent: PendingIntent = Intent(context, UpdateService::class.java).let { intent ->
+        PendingIntent.getService(context, 0, intent, 0)
+    }
     private val settings = Settings()
 
     /**
@@ -29,31 +31,61 @@ internal class UpdateManager(private val context: Context) :
      * stored preferences.
      */
     private inner class Settings {
-        val enabled: Boolean
-            get() = sharedPreferences.getBoolean(context.getString(R.string.updates_pref_key), false)
+        fun isUpdatingEnabled() = sharedPreferences.getBoolean(context.getString(R.string.updates_pref_key), true)
 
-        var interval: Long = 0L
-        var updateTimeInMillis: Long = 0L
-    }
-
-    init {
-        updaterIntent = Intent(context, UpdateService::class.java).let { intent ->
-            PendingIntent.getService(context, 0, intent, 0)
+        fun getInterval(): Long {
+            val daysBetweenUpdate = sharedPreferences.getInt(context.getString(R.string.frequency_pref_key), 1)
+            return daysBetweenUpdate * 24 * 60 * 60 * 1000L
         }
 
-        setTime()
-        setFrequency()
-        setAlarm()
+        fun getUpdateTimeInMillis(): Long {
+            val minutesAfterMidnight = sharedPreferences.getInt(context.getString(R.string.time_pref_key), 0)
+            return getTriggerTimeInMillis(minutesAfterMidnight)
+        }
+
+        /**
+         * This method will return the trigger time of the alarm in milliseconds.
+         * As the documentation of the alarms says, if we specify the trigger time that is in the past
+         * the alarm will be triggered immediately.
+         * Therefore, this method will check if the specified [minutesAfterMidnight] is in the past or not.
+         * If it is in the past, it will return the time for the next day, otherwise the time will be for the
+         * current day.
+         *
+         * **See Also:** [Alarm Documentation](https://developer.android.com/training/scheduling/alarms.html#set)
+         */
+        private fun getTriggerTimeInMillis(minutesAfterMidnight: Int): Long {
+            val hourOfDay = minutesAfterMidnight / 60
+            val minuteOfHour = minutesAfterMidnight % 60
+
+            val calendar = Calendar.getInstance().apply { timeInMillis = System.currentTimeMillis() }
+
+            val currentTime = calendar.timeInMillis
+            var triggerTime = calendar.apply {
+                set(Calendar.HOUR_OF_DAY, hourOfDay)
+                set(Calendar.MINUTE, minuteOfHour)
+            }.timeInMillis
+
+            val dayInMillis = 86_400_000
+            if (currentTime >= triggerTime) {
+                triggerTime += dayInMillis  // The time is in the past. Add 24 hour "delay".
+            }
+
+            return triggerTime
+        }
     }
 
+    /**
+     * The alarm will be automatically set whenever the preferences change.
+     * This means, there is no need to call this function.
+     */
     private fun setAlarm() {
-        if (settings.enabled) {
+        if (settings.isUpdatingEnabled()) {
             enableBootReceiver()
             Log.d(logTag, "Enabling the alarm")
             alarmManager.setInexactRepeating(
                 AlarmManager.RTC_WAKEUP,
-                settings.updateTimeInMillis,
-                settings.interval,
+                settings.getUpdateTimeInMillis(),
+                settings.getInterval(),
                 updaterIntent
             )
         } else {
@@ -63,59 +95,12 @@ internal class UpdateManager(private val context: Context) :
         }
     }
 
-    private fun setTime() {
-        val minutesAfterMidnight = sharedPreferences.getInt(context.getString(R.string.time_pref_key), 0)
-        settings.updateTimeInMillis = getTriggerTimeInMillis(minutesAfterMidnight)
-    }
-
-    /**
-     * This method will return the trigger time of the alarm in milliseconds.
-     * As the documentation of the alarms says, if we specify the trigger time that is in the past
-     * the alarm will be triggered immediately.
-     * Therefore, this method will check if the specified [minutesAfterMidnight] is in the past or not.
-     * If it is in the past, it will return the time for the next day, otherwise the time will be for the
-     * current day.
-     *
-     * **See Also:** [Alarm Documentation](https://developer.android.com/training/scheduling/alarms.html#set)
-     */
-    private fun getTriggerTimeInMillis(minutesAfterMidnight: Int): Long {
-        val hourOfDay = minutesAfterMidnight / 60
-        val minuteOfHour = minutesAfterMidnight % 60
-
-        val calendar = Calendar.getInstance().apply { timeInMillis = System.currentTimeMillis() }
-
-        val currentTime = calendar.timeInMillis
-        var triggerTime = calendar.apply {
-            set(Calendar.HOUR_OF_DAY, hourOfDay)
-            set(Calendar.MINUTE, minuteOfHour)
-        }.timeInMillis
-
-        val dayInMillis = 86_400_000
-        if (currentTime >= triggerTime) {
-            triggerTime += dayInMillis  // The time is in the past. Add 24 hour "delay".
-        }
-
-        return triggerTime
-    }
-
-    private fun setFrequency() {
-        val daysBetweenUpdate = sharedPreferences.getInt(context.getString(R.string.frequency_pref_key), 0)
-        settings.interval = daysBetweenUpdate * 24 * 60 * 60 * 1000L
-    }
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        when (key) {
-            context.getString(R.string.time_pref_key) -> setTime()
-            context.getString(R.string.frequency_pref_key) -> setFrequency()
-        }
-
         setAlarm()
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
-            setTime()
-            setFrequency()
             setAlarm()
         }
     }
