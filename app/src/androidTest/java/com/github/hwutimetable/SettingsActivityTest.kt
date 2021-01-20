7e667144570d77779a79534ed25d11481c865389
AdamTMalek <malekadas@gmail.com>
2021-01-20 12:06:37 +0000

    Add settings activity test to test for changelog preference click

---
+++ app/src/androidTest/java/com/github/hwutimetable/SettingsActivityTest.kt
@@ -0,0 +1,59 @@
+package com.github.hwutimetable
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.hwutimetable.settings.SettingsActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+@HiltAndroidTest
+class SettingsActivityTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    private lateinit var scenario: ActivityScenario<SettingsActivity>
+
+    private val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private fun launchActivity(): ActivityScenario<SettingsActivity> {
+        scenario = ActivityScenario.launch(SettingsActivity::class.java)
+        return scenario
+    }
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testRecentChangesClickShowsChangeLogDialog() {
+        launchActivity()
+        onView(withId(androidx.preference.R.id.recycler_view))
+            .perform(
+                actionOnItem<RecyclerView.ViewHolder>(
+                    hasDescendant(withText("Recent changes")), click()
+                )
+            )
+
+        onView(withSubstring(targetContext.getString(R.string.changelog_dialog_title)))
+            .check(matches(isDisplayed()))
+    }
+
+    @After
+    fun cleanup() {
+        if (this::scenario.isInitialized) {
+            scenario.close()
+        }
+    }
+}
