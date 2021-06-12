28872da20fcb91c2a5c4b1a3a86a2a90ee8c9704
AdamTMalek <malekadas@gmail.com>
2021-06-12 17:23:42 +0100

    Migrate to Kotlin DSL

--- app/src/androidTest/java/com/github/hwutimetable/HiltTestRunner.kt
+++ app/src/androidTest/java/com/github/hwutimetable/HiltTestRunner.kt
@@ -6,7 +6,7 @@ import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
 
-@Suppress("unused") // Used to run the instrumentation tests (see app's build.gradle)
+@Suppress("unused") // Used to run the instrumentation tests (see app's build.gradle.kts)
 class HiltTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
         return super.newApplication(cl, HiltTestApplication::class.java.name, context)
